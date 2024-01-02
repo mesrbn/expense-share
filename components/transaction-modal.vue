7b060602af9e0a3fc4a24c2f3264191a51acf2e7
Mehran Sarabchian <sarabchian@axxessio.com>
2024-01-02 12:48:10 +0100

    74-forms-state

--- components/transaction-modal.vue
+++ components/transaction-modal.vue
@@ -2,47 +2,64 @@
   <UModal v-model="isOpen">
     <UCard>
       <template #header> Add Transaction </template>
+      <UForm :state="state">
+        <UFormGroup
+          label="Transaction Type"
+          :required="true"
+          name="type"
+          class="mb-4"
+        >
+          <USelect
+            placeholder="Select the transaction type"
+            :options="types"
+            v-model="state.type"
+          />
+        </UFormGroup>
 
-      <UFormGroup
-        label="Transaction Type"
-        :required="true"
-        name="type"
-        class="mb-4"
-      >
-        <USelect placeholder="Select the transaction type" :options="types" />
-      </UFormGroup>
+        <UFormGroup label="Amount" :required="true" name="amount" class="mb-4">
+          <UInput
+            type="number"
+            placeholder="Amount"
+            v-model.number="state.amount"
+          />
+        </UFormGroup>
 
-      <UFormGroup label="Amount" :required="true" name="amount" class="mb-4">
-        <UInput type="number" placeholder="Amount" />
-      </UFormGroup>
+        <UFormGroup
+          label="Transaction date"
+          :required="true"
+          name="created_at"
+          class="mb-4"
+        >
+          <UInput
+            type="date"
+            icon="i-heroicons-calendar-days-20-solid"
+            v-model="state.created_at"
+          />
+        </UFormGroup>
 
-      <UFormGroup
-        label="Transaction date"
-        :required="true"
-        name="created_at"
-        class="mb-4"
-      >
-        <UInput type="date" icon="i-heroicons-calendar-days-20-solid" />
-      </UFormGroup>
+        <UFormGroup
+          label="Description"
+          hint="Optional"
+          name="description"
+          class="mb-4"
+        >
+          <UInput placeholder="Description" v-model="state.description" />
+        </UFormGroup>
 
-      <UFormGroup
-        label="Description"
-        hint="Optional"
-        name="description"
-        class="mb-4"
-      >
-        <UInput placeholder="Description" />
-      </UFormGroup>
-
-      <UFormGroup
-        :required="true"
-        label="Category"
-        name="category"
-        class="mb-4"
-      >
-        <USelect placeholder="Category" :options="categories" />
-      </UFormGroup>
-      <UButton type="submit" color="black" variant="solid" label="Save" />
+        <UFormGroup
+          :required="true"
+          label="Category"
+          name="category"
+          class="mb-4"
+        >
+          <USelect
+            placeholder="Category"
+            :options="categories"
+            v-model="state.category"
+          />
+        </UFormGroup>
+        <UButton type="submit" color="black" variant="solid" label="Save" />
+      </UForm>
     </UCard>
   </UModal>
 </template>
@@ -55,6 +72,14 @@ const props = defineProps({
   modelValue: Boolean,
 });
 const emit = defineEmits("[update:modelValue]");
+
+const state = ref({
+  type: undefined,
+  amount: 0,
+  created_at: undefined,
+  description: undefined,
+  category: undefined,
+});
 
 const isOpen = computed({
   get: () => props.modelValue,
