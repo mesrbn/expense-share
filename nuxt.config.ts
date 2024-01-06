0b8a371b835429a724f988b34a8a92373536a1a0
Mehran Sarabchian <sarabchian@axxessio.com>
2024-01-06 22:38:33 +0100

    temp fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,9 +6,6 @@ export default defineNuxtConfig({
     redirect: true,
   },
   runtimeConfig: {
-    // The private keys which are only available within server-side
-    apiSecret: "123",
-    // Keys within public, will be also exposed to the client-side
     public: {
       baseUrl: process.env.BASE_URL ?? "http://localhost:3000",
     },
