e92114622822dc39c7a3214bdee97cd632cf031e
Mehran Sarabchian <sarabchian@axxessio.com>
2024-01-06 21:07:50 +0100

    deploy config vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,12 @@ export default defineNuxtConfig({
   supabase: {
     redirect: true,
   },
+  runtimeConfig: {
+    // The private keys which are only available within server-side
+    apiSecret: "123",
+    // Keys within public, will be also exposed to the client-side
+    public: {
+      baseUrl: process.env.BASE_URL ?? "http://localhost:3000",
+    },
+  },
 });
--- pages/login.vue
+++ pages/login.vue
@@ -40,6 +40,7 @@ const email = ref("");
 const pending = ref(false);
 const { _, toastError } = useAppToast();
 const supabase = useSupabaseClient();
+const redirectUrl = useRuntimeConfig().public.baseUrl;
 
 useRedirectIfAuth("/");
 
@@ -49,7 +50,7 @@ const handleLogin = async () => {
     const { error } = await supabase.auth.signInWithOtp({
       email: email.value,
       options: {
-        emailRedirectTo: "http://localhost:3000/confirm",
+        emailRedirectTo: `${redirectUrl}/confirm`,
       },
     });
     if (error) {
